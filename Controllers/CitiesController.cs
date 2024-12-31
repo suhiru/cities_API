d0646c80b47d110b48527c14b8d15233d99dfe90
Suhiru DESILVA <suhiruben@gmail.com>
2024-12-31 16:26:20 +0100

    first commit

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CitiesController:ControllerBase
+    {
+
+        public JsonResult  JsonResult()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "New York City" },
+                new { id = 2, Name = "Antwerp" }
+            });
+        }
+    }
+}
